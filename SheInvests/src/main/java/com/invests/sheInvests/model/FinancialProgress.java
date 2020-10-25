14008369876d3930abc930560c07284f76d50381
Amy Du <amydu@Amys-MacBook-Pro.local>
2020-10-25 03:28:07 -0400

    Make models entities

--- SheInvests/src/main/java/com/invests/sheInvests/model/FinancialProgress.java
+++ SheInvests/src/main/java/com/invests/sheInvests/model/FinancialProgress.java
@@ -1,19 +1,29 @@
 package com.invests.sheInvests.model;
 
+import javax.persistence.*;
+import java.util.List;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name="financial_progress")
 public class FinancialProgress {
 
-  private static int progressId = 0;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private static int progressId;
+
+  @ManyToOne
+  @JoinColumn(name="goalid")
   private final int financialGoalId;
+  @Column(name="amountchanged")
   private final double amountChanged;
+  @Column(name="createdat")
   private final LocalDateTime createdAt;
 
   public FinancialProgress(int financialGoalId,double amountChanged, LocalDateTime createdAt) {
     this.financialGoalId = financialGoalId;
     this.amountChanged = amountChanged;
     this.createdAt = createdAt;
-    progressId += 1;
   }
 
   public double getAmountChanged() {
--- SheInvests/src/main/java/com/invests/sheInvests/model/User.java
+++ SheInvests/src/main/java/com/invests/sheInvests/model/User.java
@@ -1,33 +1,36 @@
 package com.invests.sheInvests.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.time.LocalDateTime;
 
+@Entity
 public class User {
 
-  private static int userId = 0;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private static int userId;
+  @Column(name="email")
   private final String email;
+  @Column(name="firstname")
   private final String firstName;
+  @Column(name="lastname")
   private final String lastName;
-  private FinancialGoal financialGoal;
+  @Column(name="role")
   private Roles role;
-
 
   public User(String firstName, String lastName,String email, Roles role) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
-    financialGoal = null;
     this.role = role;
-    userId += 1;
+//    userId += 1;
   }
 
-  public User(String firstName, String lastName, String email,Roles role, LocalDateTime endDate, String financialGoalName,
-              double targetAmount, String frequency) {
+  public User(String firstName, String lastName, String email,Roles role) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
-    LocalDateTime currentDate = LocalDateTime.now();
-    financialGoal = new FinancialGoal(financialGoalName,currentDate,endDate,0,targetAmount,frequency,userId);
     this.role = role;
   }
 
@@ -37,10 +40,6 @@ public class User {
 
   public String getLastName() {
     return lastName;
-  }
-
-  public FinancialGoal getFinancialGoal() {
-    return financialGoal;
   }
 
   public Roles getRole() {
